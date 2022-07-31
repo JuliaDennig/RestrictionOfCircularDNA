d291662ee533fc1260eeeec5d906bedfc7748d8b
Julia Dennig <94052845+JuliaDennig@users.noreply.github.com>
2022-07-31 14:33:35 +0200

    set up OpenApe.py and OpenEnzymeCSV.py

---
+++ OpenAPE.py
@@ -0,0 +1,25 @@
+class OpenApeFile:
+    def __init__(self, location):
+        self.location = location
+        
+    def extract_sequence(self):
+        ape_lines = []
+        with open(self.location, "r") as location:
+            ape_lines.append(location.readlines())
+        for i, elem in enumerate(ape_lines[0]):
+            if 'linear' in elem:
+                print("\nThe sequence is linear.\n")
+            elif 'circular' in elem:
+                print("The sequence is circular.\n")
+        ape_lines = ape_lines[0]
+        return ape_lines
+    
+    def get_information_from_ape_file(self, ape_lines):
+        sequence_start = ape_lines.index("ORIGIN\n") + 1
+        sequence = ''.join(ape_lines[sequence_start:])
+        sequence = sequence.replace("\n", "").replace(" ", "").replace("\t", "").replace("/", "").upper()
+        for j in range(0, 10):
+            sequence = sequence.replace(str(j), "")
+        return sequence
+
+# C:\\Users\julia\Documents\Python\RestrictionOfCircularDNA\p123.ape
---
+++ OpenEnzymeCSV.py
@@ -0,0 +1,58 @@
+TOP_ENZYMES = [
+    "AgeI-HF",
+    "BamHI-HF",
+    "BglII",
+    "ClaI",
+    "DraI",
+    "DraIII-HF",
+    "EcoRI-HF",
+    "EcoRV-HF",
+    "HindIII-HF",
+    "NcoI",
+    "NotI-HF",
+    "PstI-HF",
+    "PvuI-HF",
+    "PvuII-HF",
+    "SacI-HF",
+    "SacII",
+    "SalI-HF",
+    "ScaI-HF",
+    "XbaI",
+    "XcmI",
+    "XhoI",
+    "XmaI"
+]
+
+
+class EnzymeList:
+    def __init__(self, name, cut, type, buffer, temperature):
+        self.name = name
+        self.cut = cut
+        self.type = type
+        self.temperature = temperature
+        self.buffer = buffer
+
+    def __str__(self):
+        return "enzyme: " + self.name + ", " + self.cut + ", " + self.type + ", " + self.temperature + "°C, " + self.buffer
+
+
+class OpenEnzymeList:
+    def __init__(self, save_place):
+        self.save_place = save_place
+
+    def open_csv(self):
+        f = open(self.save_place, "r")
+        enzyme_list = f.readlines()
+        enzyme_list_all_data = []
+        for i in range(1, len(enzyme_list)):
+            el = enzyme_list[i].replace("\n", "").split(";")
+            enzyme_list_all_data.append(EnzymeList(el[0], el[1], el[2], el[3], el[4]))
+        return enzyme_list_all_data
+
+
+class FilterEnzymeListAllData:
+    def __init__(self, enzyme_list_all_data):
+        self.enzyme_list_all_data = enzyme_list_all_data
+
+    def execute_my_filter(self):
+        return list(filter(lambda enzyme: enzyme.name in TOP_ENZYMES, self.enzyme_list_all_data))
